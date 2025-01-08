492545cf20b67de17d950cb9dde3e784f09bc45d
Nebojsa Stojakovic <nebojsa.stojakovic@cake.com>
2025-01-08 13:28:12 +0100

    deleted code

--- server.js
+++ server.js
@@ -2,11 +2,6 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const { buildTree, findWord } = require("./bst");
-const {
-  buildHuffmanTree,
-  generateHuffmanCodes,
-  calculateFrequencies,
-} = require("./huffman");
 
 function readFileLines(filename) {
   return fs
@@ -22,42 +17,17 @@ const app = express();
 const words = readFileLines("dictionary.txt");
 const root = buildTree(words.sort());
 
-const wordFrequencies = calculateFrequencies(words);
-const huffmanTree = buildHuffmanTree(wordFrequencies);
-const huffmanCodes = generateHuffmanCodes(huffmanTree);
-
 app.use(express.static(path.join(__dirname, "public")));
-
-function checkWithBST(word) {
-  const found = findWord(root, word);
-  return found ? { found: true } : { found: false };
-}
-
-function checkWithHuffman(word) {
-  if (huffmanCodes[word]) {
-    return { found: true, code: huffmanCodes[word] };
-  } else {
-    return { found: false };
-  }
-}
 
 app.get("/check", (req, res) => {
   const word = (req.query.word || "").trim();
-  const algorithm = req.query.algorithm;
 
   if (!word) {
     return res.json({ error: "Unos reči je obavezan." });
   }
 
-  if (algorithm === "bst") {
-    const result = checkWithBST(word);
-    res.json(result);
-  } else if (algorithm === "huffman") {
-    const result = checkWithHuffman(word);
-    res.json(result);
-  } else {
-    res.json({ error: "Nepoznat algoritam za proveru." });
-  }
+  const result = findWord(root, word);
+  res.json(result);
 });
 
 app.listen(3000, () => {
